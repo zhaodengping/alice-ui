05d246fb13dcdcc50e07b2697add49c6bd261509
Alice Zhao <alicezhao@SHA-MC0326.local>
2020-08-11 10:41:59 +0800

    初次建立button组件

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-        Button组件
+        <Button @click="onClick">我是按钮</Button>
     </div>
 </template>
+<script lang='ts'>
+import Button from '../lib/Button.vue'
+export default {
+    components:{Button:Button},
+    setup(){
+        const onClick=()=>{
+            console.log('点击')
+        }
+        return {onClick}
+    }
+}
+</script>
