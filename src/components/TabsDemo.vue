86a75384d5445321ab67eeec903d936dcb54ee9f
Alice Zhao <alicezhao@SHA-MC0326.local>
2020-09-11 11:41:14 +0800

    vue3的bug

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,22 +1,34 @@
 <template>
-  <div>tabs</div>
-  <Tabs>
+<h1 class="title">Tabs按钮</h1>
+<Tabs v-model:selected="x">
     <Tab title='导航1'>组件1</Tab>
     <Tab title='导航2'>组件2</Tab>
-  </Tabs>
+</Tabs>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import {
+    ref
+} from 'vue'
 export default {
-    components:{
+    components: {
         Tabs,
         Tab
+    },
+    setup() {
+        const x = ref('导航2');
+        return {
+            x
+        }
     }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.title {
+    color: #1f2f3d;
+    font-weight: 400;
+}
 </style>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,51 @@
 <template>
-    <div>
-        <div v-for='(item,index) in titles' :key="index">{{item}}</div>
-        <component v-for="(c,index) in results" :key="index" :is="c"></component>
+<div class="A-tabs">
+    <div class="A-tabs-nav">
+        <div v-for='(item,index) in titles' :key="index" class="A-tabs-nav-item" @click="select(item)" :class="{selected:selected==item}">{{item}}</div>
     </div>
+    <component :is="current"></component>
+</div>
 </template>
-<script lang='ts'>
+
+<script lang="ts">
 export default {
-    setup(props,context){
-        const results=context.slots.default()
-        let titles = results.map(item=>{
+    props: {
+        selected: {
+            type: String
+        }
+    },
+    setup(props, context) {
+        let results = context.slots.default()
+        let titles = results.map(item => {
             return item.props.title
         })
-        return {results,titles}
+        let current = results.filter(item => {
+            return item.props.title == props.selected
+        })[0]
+        let select = (title: string) => {
+            context.emit('update:selected', title)
+        }
+        return {
+            results,
+            titles,
+            current,
+            select
+        }
     }
 }
 </script>
+
+<style lang="scss">
+.A-tabs-nav {
+    display: flex;
+    border-bottom: 1px solid #f2f2f2;
+
+    .A-tabs-nav-item {
+        margin-right: 10px;
+    }
+
+    .selected {
+        color: #409eff;
+    }
+}
+</style>
